6c7a593cd96c36e2b3924d31a0c832e388c6db06
Pappa <dr.pappa@gmail.com>
2020-07-07 20:59:01 +0100

    Neptune connection failing

--- api/users/graph/graph.go
+++ api/users/graph/graph.go
@@ -3,9 +3,9 @@ package main
 import (
 	"log"
 	"github.com/northwesternmutual/grammes"
-
+	"fmt"
 	"context"
-
+	"os"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/Pappa/Paloma/users/utils"
 )
@@ -13,17 +13,24 @@ import (
 type Context context.Context
 
 func Handler(ctx Context, req utils.Request) (utils.Response, error) {
+	addr, envErr := os.LookupEnv("NEPTUNE_CLUSTER_ADDRESS")
+	if envErr != true {
+		log.Println("NEPTUNE_CLUSTER_ADDRESS: not available as env var")
+    }
+	url := fmt.Sprintf("wss://%s:8182/gremlin", addr)
 
     // Creates a new client with the localhost IP.
-    client, err := grammes.DialWithWebSocket("ws://palomadbcluster-n9tmjlrotnld.cluster-c5qsk0g3zpfp.eu-west-1.neptune.amazonaws.com:8182")
+    client, err := grammes.DialWithWebSocket(url)
     if err != nil {
-        log.Fatalf("Error while creating client: %s\n", err.Error())
+		log.Fatalf("Error while creating client: %s\n", err.Error())
+		return utils.ErrorResponse(500, err), nil
     }
 
     // Executing a basic query to assure that the client is working.
     res, err := client.ExecuteStringQuery("1+3")
     if err != nil {
         log.Fatalf("Querying error: %s\n", err.Error())
+		return utils.ErrorResponse(500, err), nil
     }
 
     // Print out the result as a string
