7438070f7242023225d8e1d563fbc04aea481c45
Pappa <dr.pappa@gmail.com>
2023-02-26 21:51:43 +0000

    Setting up Github actions

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,20 +1,20 @@
-data "aws_ami" "amazon_linux_2" {
- most_recent = true
+# data "aws_ami" "amazon_linux_2" {
+#  most_recent = true
 
 
- filter {
-   name   = "owner-alias"
-   values = ["amazon"]
- }
+#  filter {
+#    name   = "owner-alias"
+#    values = ["amazon"]
+#  }
 
 
- filter {
-   name   = "name"
-   values = ["amzn2-ami-hvm*"]
- }
-}
+#  filter {
+#    name   = "name"
+#    values = ["amzn2-ami-hvm*"]
+#  }
+# }
 
-resource "aws_instance" "graph" {
-  ami           = data.aws_ami.amazon_linux_2.id
-  instance_type = "t3.micro"
-}
+# resource "aws_instance" "graph" {
+#   ami           = data.aws_ami.amazon_linux_2.id
+#   instance_type = "t3.micro"
+# }
