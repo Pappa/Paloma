5b63ca75b3286de7a39e00c10e86ea4766d4e4fd
Pappa <dr.pappa@gmail.com>
2023-02-27 19:52:48 +0000

    ECS task definition

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -1,23 +1,9 @@
 resource "aws_ecs_task_definition" "paloma_db" {
   family                   = "paloma_db"
   network_mode             = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
+  requires_compatibilities = ["ECS"]
   cpu                      = 1024
   memory                   = 2048
 
-  container_definitions = jsonencode([
-    {
-      image       = "docker.io/orientdb:latest"
-      cpu         = 1024
-      memory      = 2048
-      name        = "paloma_db"
-      networkMode = "awsvpc"
-      portMappings = [
-        {
-          containerPort = 3000
-          hostPort      = 3000
-        }
-      ]
-    }
-  ])
+  container_definitions = file("ecs-task-definitions/paloma_db.json")
 }
