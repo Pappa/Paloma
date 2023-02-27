0abda4d23e628b29469561103812232e34f9be2f
Pappa <dr.pappa@gmail.com>
2023-02-27 19:45:57 +0000

    ECS task definition

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,24 +1,24 @@
 terraform {
 
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-        }
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
     }
+  }
 
-    backend "s3" {
-        bucket = "dung-beetle-bash-terraform-backend"
-        key    = "paloma"
-        region = "eu-west-1"
-    }
-    
+  backend "s3" {
+    bucket = "dung-beetle-bash-terraform-backend"
+    key    = "paloma"
+    region = "eu-west-1"
+  }
+
 }
 
 provider "aws" {
- region = "eu-west-1"
- default_tags {
-   tags = {
-     Project     = "Paloma"
-   }
- }
+  region = "eu-west-1"
+  default_tags {
+    tags = {
+      Project = "Paloma"
+    }
+  }
 }
