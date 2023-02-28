ca4ef970d46bf45c788215b2317b7ee7d1a9694d
Pappa <dr.pappa@gmail.com>
2023-02-28 20:21:43 +0000

    ECS task definition

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,19 +1,22 @@
 resource "aws_iam_role" "ecs_task_execution" {
   name               = "paloma_db_ecs_task_execution"
   assume_role_policy = file("policies/assume-role/ecs_task_execution.json")
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy",
+    aws_iam_policy.ecs_logs.arn
+  ]
 }
 
 resource "aws_iam_role" "ecs_task" {
   name               = "paloma_db_ecs_task"
   assume_role_policy = file("policies/assume-role/ecs_task.json")
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/AmazonS3FullAccess",
+    aws_iam_policy.ecs_logs.arn
+  ]
 }
 
-resource "aws_iam_role_policy_attachment" "ecs_task_execution" {
-  role       = aws_iam_role.ecs_task_execution.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
-
-resource "aws_iam_role_policy_attachment" "ecs_task" {
-  role       = aws_iam_role.ecs_task.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+resource "aws_iam_policy" "ecs_logs" {
+    name = "paloma_ecs_logs"
+    policy = file("policies/ecs_logs.json")
 }
