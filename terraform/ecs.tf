8f529c005a04daa7017070477c45e9286ca43295
Pappa <dr.pappa@gmail.com>
2023-02-28 17:06:39 +0000

    ECS task definition

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -1,9 +1,9 @@
 resource "aws_ecs_task_definition" "paloma_db" {
   family                   = "paloma_db"
   network_mode             = "awsvpc"
-  requires_compatibilities = ["EC2"]
+  requires_compatibilities = ["FARGATE"]
   cpu                      = 1024
   memory                   = 2048
-  
+
   container_definitions = file("ecs-task-definitions/paloma_db.json")
 }
