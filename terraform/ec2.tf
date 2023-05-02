ca981e365ae2082bc91bed07e259b7a1ebb08b89
Pappa <dr.pappa@gmail.com>
2023-05-02 20:26:59 +0100

    Adding ecsInstanceProfile

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -43,7 +43,7 @@ data "aws_ami" "aws_optimized_ecs" {
 resource "aws_instance" "graph" {
   ami                  = data.aws_ami.aws_optimized_ecs.id
   instance_type        = "t3.micro"
-  iam_instance_profile = aws_iam_instance_profile.ecs_instance_profile.arn
+  iam_instance_profile = aws_iam_instance_profile.ecs_instance_profile.name
   user_data            = <<EOF
 #!/bin/bash
 echo "ECS_CLUSTER=${aws_ecs_cluster.paloma.name}" >> /etc/ecs/ecs.config
