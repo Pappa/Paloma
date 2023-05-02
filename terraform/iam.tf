bb41e6c3e9dd92a7d36fe5085e4ae5f7db638eb3
Pappa <dr.pappa@gmail.com>
2023-05-02 20:09:01 +0100

    Adding ecsInstanceRole

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -45,3 +45,24 @@ data "aws_iam_policy_document" "ecs_logs" {
     ]
   }
 }
+
+data "aws_iam_policy_document" "ecs_instance_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      identifiers = ["ec2.amazonaws.com", "ecs-tasks.amazonaws.com"]
+      type        = "Service"
+    }
+  }
+}
+
+resource "aws_iam_role" "ecs_instance_role" {
+  name               = "ecsInstanceRole"
+  assume_role_policy = data.aws_iam_policy_document.ecs_instance_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_instance_role" {
+  role       = aws_iam_role.ecs_instance_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+}
